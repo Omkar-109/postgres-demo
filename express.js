31f5cd7436f5985b443479177d364fe6965e6d1c
Omkar-109 <127653970+Omkar-109@users.noreply.github.com>
2024-12-24 17:13:06 +0530

    added some pg part

--- express.js
+++ express.js
@@ -1,19 +1,48 @@
 import express from "express"
 import bodyParser from "body-parser"
+import pg from "pg";
+
+const db = new pg.Client({
+  user: "postgres",
+  host: "localhost",
+  database: "",
+  password: "",
+  port: 5432,
+});
 
 const app = express()
 app.use(express.static("public")) 
 app.use(bodyParser.urlencoded({extended:true}))
 
-app.get('/', (request, response) => {
+db.connect();
+
+app.get('/', async (request, response) => {
     //fetch database
+
+    try {
+        const result = await db.query('SELECT * FROM studenttable');
+        response.status(200).json(result.rows);
+      } catch (err) {
+        console.error(err.message);
+        response.status(500).send('Server Error');
+      }
+
     response.render("home.ejs")  
 
 })
 
-app.post('/addstudent', (request, response) => {
-     data=request.body
-     console.log(data)
+app.post('/addstudent', async (request, response) => {
+    
+    const { rollno, name } = request.body
+    try{
+        const result=await db.query('INSERT INTO studenttable (rollno, s_name) VALUES ($1, $2) RETURNING *',
+            [rollno, name]);
+        console.log(result)
+    
+    } catch(err){
+        console.error(err)
+    }
+     
      //add to database
 })
 
